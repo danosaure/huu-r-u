1d9719a9cb34227952b094bcdc1939b0a2cab9b6
Danosaure <1444063+danosaure@users.noreply.github.com>
2024-02-24 02:29:32 -0500

    - #6: Define paths by name in registry.

--- src/api-server/registry.ts
+++ src/api-server/registry.ts
@@ -3,6 +3,7 @@ import { ApiServerError } from "./error";
 export type ControllerType = (...args: any[]) => Promise<void>;
 
 const CONTROLLERS: Record<string, ControllerType> = {};
+const PATH_NAMES: Record<string, string> = {};
 
 export class RegistryError extends ApiServerError {
   constructor(message: string) {
@@ -11,11 +12,19 @@ export class RegistryError extends ApiServerError {
   }
 }
 
-export const register = (path: string, controller: ControllerType): void => {
+export const register = (
+  name: string,
+  path: string,
+  controller: ControllerType
+): void => {
+  if (PATH_NAMES[name]) {
+    throw new RegistryError(`Path name "${name}" already used.`);
+  }
   if (CONTROLLERS[path]) {
     throw new RegistryError(`Path "${path}" already used.`);
   }
   CONTROLLERS[path] = controller;
+  PATH_NAMES[name] = path;
 };
 
 export const api = (path: string): ControllerType => {
@@ -25,4 +34,12 @@ export const api = (path: string): ControllerType => {
     return controller;
   }
   throw new RegistryError(`Controller path "${path}" not found.`);
+};
+
+export const byName = (name: string): ControllerType => {
+  const path = PATH_NAMES[name];
+  if (path) {
+    return api(path);
+  }
+  throw new RegistryError(`Controller named "${name}" not found.`);
 };
